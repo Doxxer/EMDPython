dd7a72b557ca5dbd7d3cdd96aaf3a4e738a3d35c
Timur Turaev <doxxeracm@gmail.com>
2015-04-27 18:36:47 +0300

    Reformat json to txt

--- playground.py
+++ playground.py
@@ -1,6 +1,30 @@
+from json import loads
+
 __author__ = 'doxer'
 
-n = 13
-a = 0 if n < 8 else float('nan')
+file = 'full'
+input_json = 'data/' + file + '.json'
+output_txt = 'data/' + file + '.txt'
 
-print a
+with open(input_json) as jsonFile:
+    json_data = loads(jsonFile.read())
+
+
+def parse_gesture(gesture):
+    tag = gesture['tag']
+    x = gesture['x']
+    y = gesture['y']
+    t = gesture['t']
+    fingers = 1
+    strokes = len(set(gesture['id']))
+
+    with open(output_txt, mode='a') as f:
+        f.write("{0}\n".format(' '.join(str(s) for s in (tag, fingers, strokes, len(x)))))
+        for px, py, pt in zip(x, y, t):
+            f.write("{0} {1} {2}\n".format(px, py, pt))
+
+
+parse_gesture(json_data[0])
+
+for gesture in json_data:
+    parse_gesture(gesture)
