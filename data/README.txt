5ded8ccb6b47a998f092f2f509ee860554e99892
Timur Turaev <doxxeracm@gmail.com>
2015-04-30 14:12:42 +0300

    stroke id

---
+++ data/README.txt
@@ -0,0 +1,8 @@
+name format: nGestures_nSamples_{S, M, SM}.json
+	* nGestures - count of distinct gesture patterns
+	* nSamples - count of repeats for one gesture sample
+	* S - single finger (onetouch), M - multitouch, SM - both of them
+
+File 'h.json' -- 2 gestures
+	* letter H ---- |-| 		(3 different strokes)
+	* letter H ---- || and -	(2 parallel strokes and one horizontal)
--- gestureParser.py
+++ gestureParser.py
@@ -2,7 +2,7 @@ from json import loads
 
 __author__ = 'doxer'
 
-file = 'full'
+file = '20_5_SM'
 input_json = 'data/' + file + '.json'
 output_txt = 'data/' + file + '.txt'
 
@@ -13,7 +13,7 @@ st = {'O': 1, 'ok': 1, '1': 1, '2': 1, '3': 1, '4': 2, '5': 2, '6': 1, '9': 1, '
       '3v': 3, '3h': 3, '3vh': 6, '2v_diff': 2, '2w': 2, '2w_diff': 2, '2v_up': 2}
 
 fin = {'O': 1, 'ok': 1, '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '9': 1, 'z': 1, '2v': 2, '2vh': 2, '2h': 2,
-     '3v': 3, '3h': 3, '3vh': 3, '2v_diff': 1, '2w': 2, '2w_diff': 1, '2v_up': 2}
+     '3v': 3, '3h': 3, '3vh': 3, '2v_diff': 1, '2w': 2, '2w_diff': 1, '2v_up': 2, '+' : 1, 'B' : 1, 'W' : 1, 'x' : 1, 'o' : 1, '3hv' : 3}
 
 
 def parse_gesture(gesture):
@@ -21,18 +21,21 @@ def parse_gesture(gesture):
     x = gesture['x']
     y = gesture['y']
     t = gesture['t']
-    if tag not in fin or tag not in st:
+    sid = gesture['id']
+    if tag not in fin:
         print "ERROR " + tag
         return
 
     fingers = fin[tag]
-    strokes = st[tag]
+    strokes = len(set(sid))
 
     with open(output_txt, mode='a') as f:
         f.write("{0}\n".format(' '.join(str(s) for s in (tag, fingers, strokes, len(x)))))
-        for px, py, pt in zip(x, y, t):
-            f.write("{0} {1} {2}\n".format(px, py, pt))
+        for px, py, pt, psid in zip(x, y, t, sid):
+            f.write("{0} {1} {2} {3}\n".format(px, py, pt, psid))
 
+
+print json_data[0].keys()
 
 for gesture in json_data:
     parse_gesture(gesture)
